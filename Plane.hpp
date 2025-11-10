0afc14a0bc3eaf08e1a34f9aeb0f915ef5888a49
AlexA <alexandre.abeele@student.junia.com>
2025-11-10 22:07:25 +0100

    Added base methods and attributes to the class Plane

--- Plane.hpp
+++ Plane.hpp
@@ -5,8 +5,26 @@
 #include <chrono>
 #include <cmath>
 
+using std::chrono::duration;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+using namespace std::chrono_literals;
+
 class Plane {
 protected:
 	std::string nom;
+	float x_ = 0.;
+	float y_ = 0.;
+	float angle_ = 0;
+	// float radius_ = 0;       A vérifier si les avions tournent en rond ou pas 
+	float speed_ = 0;
+	float altitude_ = 0.;
+public:
+	Plane();
 
+	float get_x();
+	float get_y();
+	float get_angle();
+
+	~Plane();
 };
