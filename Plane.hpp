e6ceca358206996461bc83e7e32f0f0199418ad0
AlexA <alexandre.abeele@student.junia.com>
2025-11-10 20:52:26 +0100

    Added base files

---
+++ Plane.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cmath>
+
+class Plane {
+protected:
+	std::string nom;
+
+};
--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
 
+int main(void) {
+
+}
