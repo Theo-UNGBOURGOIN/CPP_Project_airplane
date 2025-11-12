9931ed9bc77ebecc35c22be5d0054a74ebc38f49
AlexA <alexandre.abeele@student.junia.com>
2025-11-12 15:22:33 +0100

    Update

--- Plane.hpp
+++ Plane.hpp
@@ -10,21 +10,30 @@ using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
 using namespace std::chrono_literals;
 
+class Plane;
+
+void update_plane(Plane& plane, bool& stop_thread);
+
 class Plane {
-protected:
+	std::thread t_;
 	std::string nom;
 	float x_ = 0.;
 	float y_ = 0.;
-	float angle_ = 0;
+	float trajectoire_ = 0;
 	// float radius_ = 0;       A vérifier si les avions tournent en rond ou pas 
 	float speed_ = 0;
 	float altitude_ = 0.;
+	float carburant = 0;
+	float consommation = 0;
+	bool stop_thread_ = false;
+	high_resolution_clock::time_point initial_clock_;
 public:
-	Plane();
+	Plane(const float& angle, const float& altitude, const float& speed);
 
+	void update();
 	float get_x();
 	float get_y();
-	float get_angle();
+	float get_traj();
 
 	~Plane();
 };
--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
 #include <cstdlib>
-#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <vector>
 #include <memory>
+#include <SFML/Graphics.hpp>
+
+#include "Plane.hpp"
+
+#ifdef _MSC_VER
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#define _PATH_IMG_ "C:/Program Files/SFML/img/"
+#else
+// #define _PATH_IMG_ "../img/"
+#define _PATH_IMG_ "./img/"
+#endif
+
+const std::string path_image(PATH_IMG);
 
 int main(void) {
 
