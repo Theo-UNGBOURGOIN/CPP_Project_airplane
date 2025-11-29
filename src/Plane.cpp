9f348a4703aa4700f880b6522608f0575bc5bca4
AlexA <alexandre.abeele@student.junia.com>
2025-11-29 22:14:06 +0100

    Modified mutex to correctly lock everything

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -7,6 +7,8 @@
 // void requestlanding 
 // void takeof 
 
+std::mutex globalCoutMutex;
+
 Plane::Plane(const std::string& name, const float& speed, APP* target, TWR* spawn, std::mutex& mtx) : target_(target), Agent(name, mtx), speed_(speed) {
 	Position pos;
 	pos.altitude_ = 0; // altitude de départ
@@ -23,13 +25,19 @@ Plane::Plane(const std::string& name, const float& speed, APP* target, TWR* spaw
 };
 
 void Plane::run() {
-	std::cout << "RUN PLANE" << std::endl;
+	{
+		std::lock_guard<std::mutex> lock(globalCoutMutex);
+		std::cout << "RUN PLANE" << std::endl;
+	}
 	while (isRunning()) {
 		pos_.x_ += trajectory_.x_ * speed_ * 0.1f;
 		pos_.y_ += trajectory_.y_ * speed_ * 0.1f;
-		mtx_.lock();
-		std::cout << "Pos: " << name_ << "   " << (*this).fgetpos().x_ << "   " << (*this).fgetpos().y_ << "   " << (*this).fgetpos().altitude_ << std::endl;
-		mtx_.unlock(); 
+		std::ostringstream os;
+		os << "Pos: " << name_ << "   " << (*this).fgetpos().x_ << "   " << (*this).fgetpos().y_ << "   " << (*this).fgetpos().altitude_ << std::endl;
+		{
+			std::lock_guard<std::mutex> lock(globalCoutMutex);
+			std::cout << os.str();
+		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		
 		Position currentPos = fgetpos();
@@ -44,7 +52,10 @@ void Plane::run() {
 		}
 	}
 
-	std::cout << "Plane " << name_ << " LANDED" << std::endl;
+	{
+		std::lock_guard<std::mutex> lock(globalCoutMutex);
+		std::cout << "Plane " << name_ << " LANDED" << std::endl;
+	}
 };
 
 void Plane::requestlanding(APP *target) {
